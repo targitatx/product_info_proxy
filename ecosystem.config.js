37f31ef39d0aa928032ff2dab802a7bc36307731
Maggie <maggiejgray@gmail.com>
2019-04-11 14:37:07 -0500

    setup pm2 ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'targitProxy',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-3-16-128-154.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/targit.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/targitatx/product_info_proxy.git',
+      path: '/home/ubuntu/targitProxy',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
